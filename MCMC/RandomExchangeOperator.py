19b5ed23b9df009e5d8ab1c308f02514807df072
farrisric <61943289+farrisric@users.noreply.github.com>
2022-01-05 15:15:08 +0100

    Ads sites opt

--- MCMC/RandomExchangeOperator.py
+++ MCMC/RandomExchangeOperator.py
@@ -33,7 +33,21 @@ class RandomExchangeOperator:
         particle.swap_symbols(zip(symbol1_indices, symbol2_indices))
         return list(zip(symbol1_indices, symbol2_indices))
 
-        
+    def random_adsorbate_migration(self, ads_sites_list, number_of_ads, shared_atoms_by_sites):
+        adsorbate_sites_indices = np.arange(len(ads_sites_list))
+
+        initial_sites_indices = []
+        initial_sites_atoms = set()
+
+        while len(initial_sites_indices) < number_of_ads and len(adsorbate_sites_indices) > 0 :
+            random_site_index = np.random.choice(adsorbate_sites_indices)
+            random_site_atoms = set(ads_sites_list[random_site_index])
+            if len(initial_sites_atoms.intersection(random_site_atoms)) < shared_atoms_by_sites:
+                initial_sites_indices.append(random_site_index)
+                initial_sites_atoms = initial_sites_atoms.union(random_site_atoms)
+            adsorbate_sites_indices = np.delete(adsorbate_sites_indices, np.where(adsorbate_sites_indices == random_site_index)[0])
+        return initial_sites_atoms
+
     def random_exchange_plus_adsorbate_migration(self, particle, adsorbate):
         n_exchanges = min(np.random.geometric(p=self.p_geometric, size=1)[0], self.max_exchanges)
         symbol1_indices = np.random.choice(particle.get_indices_by_symbol(self.symbol1), n_exchanges, replace=False)
