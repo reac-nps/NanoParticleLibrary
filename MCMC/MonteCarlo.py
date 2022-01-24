8e49f532432f819aeaf3e8ee63f41fd668adc3c9
farrisric <farrisric@outlook.com>
2022-01-24 11:11:37 +0100

    MonteCarlo with Adsorbates

--- MCMC/MonteCarlo.py
+++ MCMC/MonteCarlo.py
@@ -190,7 +190,7 @@ def run_monte_carlo_for_adsorbates(beta, max_steps, start_particle, energy_calcu
             print("Step: {}".format(total_steps))
             print("Lowest energy: {}".format(lowest_energy))
 
-        exchanges = exchange_operator.random_adsorbate_migration(start_particle)
+        exchanges = exchange_operator.coupled_random_exchange(start_particle)
 
         accepted_particle = copy.deepcopy(start_particle)
 
@@ -240,104 +240,103 @@ def run_monte_carlo_for_adsorbates(beta, max_steps, start_particle, energy_calcu
     return [best_particle, accepted_energies]
 
 
-    def run_monte_carlo_ordering_adsorbates(beta, max_steps, start_particle, ordering_energy_calculator, adsorbates_energy_calculato, n_adsorbates):
-        from Core.GlobalFeatureClassifier import AdsorptionFeatureVector
+def run_monte_carlo_ordering_adsorbates(beta, max_steps, start_particle, ordering_energy_calculator, adsorbates_energy_calculator, n_adsorbates, local_feature_classifier):
+    from Core.GlobalFeatureClassifier import AdsorptionFeatureVector
 
-        adsorbates_exchange_operator = RandomExchangeOperator(0.5)
-        adsorbates_exchange_operator.bind_adsorbates(start_particle, n_adsorbates)
+    exchange_operator = RandomExchangeOperator(0.5)
+    exchange_operator.bind_adsorbates(start_particle, n_adsorbates)
 
-        symbols = start_particle.get_all_symbols()
-        adsorbates_energy_key = energy_calculato_adsobates.get_energy_key()
+    symbols = start_particle.get_all_symbols()
+    adsorbates_energy_key = adsorbates_energy_calculator.get_energy_key()
 
-        adsorbate_feature_classifier = AdsorptionFeatureVector(symbols)
+    adsorbate_feature_classifier = AdsorptionFeatureVector(symbols)
+    adsorbate_feature_classifier.compute_feature_vector(start_particle)
+    adsorbates_energy_calculator.compute_energy(start_particle)
+
+    ordering_energy_key, local_env_calculator, ordering_exchange_operator = setup_monte_carlo(start_particle, ordering_energy_calculator, local_feature_classifier)
+
+    initial_adsorbed_sites = start_particle.get_indices_of_adsorbates()
+
+    def get_ordering_and_adsorbates_energy(particle):
+        ordering_energy = start_particle.get_energy(ordering_energy_key)
+        adsorbates_energy = start_particle.get_energy(adsorbates_energy_key)
+        particle.set_energy('TOT', ordering_energy + adsorbates_energy)
+        return particle.get_energy('TOT')
+
+    #start_energy = start_particle.get_energy(ordering_energy_key) + start_particle.get_energy(adsorbates_energy_key)
+    start_energy = get_ordering_and_adsorbates_energy(start_particle)
+    
+    lowest_energy = start_energy
+    sites_occupied = start_particle.get_indices_of_adsorbates()
+    accepted_energies = [(lowest_energy, 0, sites_occupied)]
+
+    found_new_solution = False
+    fields = ['energies', 'symbols', 'positions']
+    best_particle = copy.deepcopy(start_particle.get_as_dictionary(fields))
+
+    total_steps = 0
+    no_improvement = 0
+    while no_improvement < max_steps:
+        total_steps += 1
+        if total_steps % 2000 == 0:
+            print("Step: {}".format(total_steps))
+            print("Lowest energy: {}".format(lowest_energy))
+
+        ordering_exchanges, adsorbates_exchanges = exchange_operator.coupled_random_exchange(start_particle)
+
+        start_particle, neighborhood = update_atomic_features(ordering_exchanges, local_env_calculator, local_feature_classifier,
+                                                              start_particle)
+
+        accepted_particle = copy.deepcopy(start_particle)
         adsorbate_feature_classifier.compute_feature_vector(start_particle)
-        energy_calculato_adsobates.compute_energy(start_particle)
 
-        ordering_energy_key, local_env_calculator, ordering_exchange_operator = setup_monte_carlo(start_particle, ordering_energy_calculator, local_feature_classifier)
+        ordering_energy_calculator.compute_energy(start_particle)
+        adsorbates_energy_calculator.compute_energy(start_particle)
+        new_energy = get_ordering_and_adsorbates_energy(start_particle)
 
+        delta_e = new_energy - start_energy
 
-        initial_adsorbed_sites = start_particle.get_indices_of_adsorbates()
-        
-        def get_ordering_and_adsorbates_energy(particle):
-            ordering_energy = start_particle.get_energy(ordering_energy_key)
-            adsorbates_energy = start_particle.get_energy(adsorbates_energy_key)
-            particle.set_energy('TOT', ordering_energy + adsorbates_energy)
-            return particle.get_energy('TOT')
+        acceptance_rate = min(1, np.exp(-beta * delta_e))
+        if np.random.random() < acceptance_rate:
+            if found_new_solution:
+                if new_energy > start_energy:
+                    start_particle.swap_symbols(ordering_exchanges)
+                    start_particle.swap_status(adsorbates_exchanges)
+                    best_particle = copy.deepcopy(start_particle.get_as_dictionary(fields))
+                    best_particle['energies'][ordering_energy_key] = copy.deepcopy(start_energy)
+                    best_particle['ads'] = copy.deepcopy(start_particle.get_indices_of_adsorbates())
+                    start_particle.swap_status(adsorbates_exchanges)
+                    start_particle.swap_symbols(ordering_exchanges)
 
-        #start_energy = start_particle.get_energy(ordering_energy_key) + start_particle.get_energy(adsorbates_energy_key)
-        start_energy = get_ordering_and_adsorbates_energy(start_particle)
-        lowest_energy = start_energy
-        sites_occupied = start_particle.get_indices_of_adsorbates()
-        accepted_energies = [(lowest_energy, 0, sites_occupied)]
+            sites_occupied = start_particle.get_indices_of_adsorbates()
+            start_energy = new_energy
+            accepted_energies.append((new_energy, total_steps, sites_occupied))
 
-        found_new_solution = False
-        fields = ['energies', 'symbols', 'positions']
-        best_particle = copy.deepcopy(start_particle.get_as_dictionary(fields))
-
-        total_steps = 0
-        no_improvement = 0
-        while no_improvement < max_steps:
-            total_steps += 1
-            if total_steps % 2000 == 0:
-                print("Step: {}".format(total_steps))
-                print("Lowest energy: {}".format(lowest_energy))
-
-            adsorbates_exchanges = adsorbates_exchange_operator.random_adsorbate_migration(start_particle)
-            ordering_exchanges = ordering_exchange_operator.random_exchange(start_particle)
-
-            start_particle, neighborhood = update_atomic_features(exchanges, local_env_calculator, local_feature_classifier,
-                                                                  start_particle)
-
-            accepted_particle = copy.deepcopy(start_particle)
-            global_feature_classifier.compute_feature_vector(start_particle)
-            ordering_energy_calculator.compute_energy(start_particle)
-            adsorbates_energy_calculator.compute_energy(start_particle)
-            new_energy = get_ordering_and_adsorbates_energy(start_particle)
-
-
-            delta_e = new_energy - start_energy
-
-            acceptance_rate = min(1, np.exp(-beta * delta_e))
-            if np.random.random() < acceptance_rate:
-                if found_new_solution:
-                    if new_energy > start_energy:
-                        start_particle.swap_symbols(ordering_exchanges)
-                        start_particle.swap_status(adsorbates_exchanges)
-                        best_particle = copy.deepcopy(start_particle.get_as_dictionary(fields))
-                        best_particle['energies'][energy_key] = copy.deepcopy(start_energy)
-                        best_particle['ads'] = copy.deepcopy(start_particle.get_indices_of_adsorbates())
-                        start_particle.swap_status(adsorbates_exchanges)
-                        start_particle.swap_symbols(ordering_exchanges)
-
-                sites_occupied = start_particle.get_indices_of_adsorbates()
-                start_energy = new_energy
-                accepted_energies.append((new_energy, total_steps, sites_occupied))
-
-                if new_energy < lowest_energy:
-                    no_improvement = 0
-                    lowest_energy = new_energy
-                    found_new_solution = True
-                else:
-                    no_improvement += 1
-                    found_new_solution = False
-
+            if new_energy < lowest_energy:
+                no_improvement = 0
+                lowest_energy = new_energy
+                found_new_solution = True
             else:
                 no_improvement += 1
+                found_new_solution = False
 
-                # roll back exchanges and make sure features and environments are up-to-date
-                start_particle.swap_status(adsorbates_exchanges)
-                start_particle.swap_symbols(ordering_exchanges)
-                start_particle.set_energy('TOT', start_energy)
+        else:
+            no_improvement += 1
 
-                for index in neighborhood:
-                    local_env_calculator.compute_local_environment(start_particle, index)
-                    local_feature_classifier.compute_atom_feature(start_particle, index)
+            # roll back exchanges and make sure features and environments are up-to-date
+            start_particle.swap_status(adsorbates_exchanges)
+            start_particle.swap_symbols(ordering_exchanges)
+            start_particle.set_energy('TOT', start_energy)
 
-                if found_new_solution:
-                    best_particle = copy.deepcopy(start_particle.get_as_dictionary(fields))
-                    best_particle['energies'][energy_key] = copy.deepcopy(start_energy)
-                    best_particle['ads'] = copy.deepcopy(start_particle.get_indices_of_adsorbates())
-                    found_new_solution = False
+            for index in neighborhood:
+                local_env_calculator.compute_local_environment(start_particle, index)
+                local_feature_classifier.compute_atom_feature(start_particle, index)
 
-        accepted_energies.append((accepted_energies[-1][0], total_steps))
-        return [best_particle, accepted_energies]
+            if found_new_solution:
+                best_particle = copy.deepcopy(start_particle.get_as_dictionary(fields))
+                best_particle['energies'][ordering_energy_key] = copy.deepcopy(start_energy)
+                best_particle['ads'] = copy.deepcopy(start_particle.get_indices_of_adsorbates())
+                found_new_solution = False
+
+    accepted_energies.append((accepted_energies[-1][0], total_steps))
+    return [best_particle, accepted_energies]
--- MCMC/RandomExchangeOperator.py
+++ MCMC/RandomExchangeOperator.py
@@ -56,3 +56,21 @@ class RandomExchangeOperator:
 
         particle.swap_status(zip(status0_indices, status1_indices))
         return list(zip(status0_indices, status1_indices))
+
+    def coupled_random_exchange(self, particle):
+        probability = np.random.randint(2)
+        n_exchanges = min(np.random.geometric(p=self.p_geometric, size=1)[0], self.max_exchanges)
+        symbol1_indices = np.random.choice(particle.get_indices_by_symbol(self.symbol1), n_exchanges*probability, replace=False)
+        symbol2_indices = np.random.choice(particle.get_indices_by_symbol(self.symbol2), n_exchanges*probability, replace=False)
+
+        ordering_exchange = list(zip(symbol1_indices, symbol2_indices))
+        particle.swap_symbols(ordering_exchange)
+
+        n_exchanges = min(np.random.geometric(p=self.p_geometric, size=1)[0], self.max_adsorbate_exchanges)
+        status0_indices = np.random.choice(particle.get_occupation_status_by_indices(0), n_exchanges*(1-probability), replace=False)
+        status1_indices = np.random.choice(particle.get_occupation_status_by_indices(1), n_exchanges*(1-probability), replace=False)
+
+        adsorbate_exchange = list(zip(status0_indices, status1_indices))
+        particle.swap_status(adsorbate_exchange)
+
+        return ordering_exchange, adsorbate_exchange
