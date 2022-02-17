6c53d10f156f3fe9cc12811dd5aaa992f2d00978
farrisric <farrisric@outlook.com>
2022-02-17 12:03:33 +0100

    Reworked TOP descriptor

--- Core/GlobalFeatureClassifier.py
+++ Core/GlobalFeatureClassifier.py
@@ -77,7 +77,6 @@ class SimpleFeatureClassifier(GlobalFeatureClassifier):
 
         return n_aa_bonds, n_bb_bonds, n_ab_bonds
 
-
 class TopologicalFeatureClassifier(SimpleFeatureClassifier):
     """Classifier for a generalization of the topological descriptors by Kozlov et al. (2015).
 
@@ -87,27 +86,28 @@ class TopologicalFeatureClassifier(SimpleFeatureClassifier):
     def __init__(self, symbols):
         SimpleFeatureClassifier.__init__(self, symbols)
         self.feature_key = 'TFC'
+        self.bond_scaling_factor = 1
 
     def compute_feature_vector(self, particle):
         n_atoms = particle.get_n_atoms()
         n_aa_bonds, n_bb_bonds, n_ab_bonds = self.compute_respective_bond_counts(particle)
-        coordinated_atoms = [len(particle.get_atom_indices_from_coordination_number([cn], symbol=self.symbol_a)) for cn in range(13)]
+        
+        coordinated_atoms_a = [len(particle.get_atom_indices_from_coordination_number([cn], symbol=self.symbol_a)) for cn in range(13)]
+        coordinated_atoms_b = [len(particle.get_atom_indices_from_coordination_number([cn], symbol=self.symbol_b)) for cn in range(13)]
 
-        M = particle.get_stoichiometry()[self.symbol_a]*0.1
-
-        feature_vector = np.array([n_aa_bonds/n_atoms, n_bb_bonds/n_atoms, n_ab_bonds/n_atoms, M] + coordinated_atoms)
+        feature_vector = np.array([n_aa_bonds*self.bond_scaling_factor, n_bb_bonds*self.bond_scaling_factor,
+                                   n_ab_bonds*self.bond_scaling_factor] + coordinated_atoms_a + coordinated_atoms_b)
+        
         particle.set_feature_vector(self.feature_key, feature_vector)
-
-
 
 
 class TopologicalEnvironmentFeatureClassifier(SimpleFeatureClassifier):
     def __init__(self, symbols):
         SimpleFeatureClassifier.__init__(self, symbols)
         symbols_copy = copy.deepcopy(symbols)
-        symbols_copy.sort()
-        self.symbols = symbols_copy
+        self.symbols = sorted(symbols_copy)
         self.feature_key = 'TEC'
+        
         self.coordination_number_offsets = [int(cn*(cn + 1)/2) for cn in range(13)]
     
     def compute_n_features(self, particle):
